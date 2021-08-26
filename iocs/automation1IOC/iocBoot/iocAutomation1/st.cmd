258cab5c085ac3198034212337a0be30c88dd372
Kevin Peterson <kmpeters@anl.gov>
2021-08-26 10:58:53 -0500

    Added automation1.cmd, sourced from st.cmd, which is easier to include in an existing IOC than st.cmd.automation1

--- iocs/automation1IOC/iocBoot/iocAutomation1/st.cmd
+++ iocs/automation1IOC/iocBoot/iocAutomation1/st.cmd
@@ -2,23 +2,30 @@
 
 < envPaths
 
-cd "${TOP}"
-
 ## Register all support components
-dbLoadDatabase "dbd/automation1.dbd"
-automation1_registerRecordDeviceDriver pdbbase
-
-cd "${TOP}/iocBoot/${IOC}"
-
+dbLoadDatabase("../../dbd/automation1.dbd")
+automation1_registerRecordDeviceDriver(pdbbase)
 
 ## motorUtil (allstop & alldone)
 dbLoadRecords("$(MOTOR)/db/motorUtil.db", "P=automation1:")
 
-##
+## Automation1 Support
+< automation1.cmd
 
 iocInit
 
 ## motorUtil (allstop & alldone)
 motorUtilInit("automation1:")
+
+##
+# This IOC does not use save/restore, so set values of some PVs
+dbpf("Auto1:m1.RTRY", "0")
+dbpf("Auto1:m1.TWV", "0.1")
+dbpf("Auto1:m2.RTRY", "0")
+dbpf("Auto1:m2.TWV", "0.1")
+dbpf("Auto1:m3.RTRY", "0")
+dbpf("Auto1:m3.TWV", "0.1")
+dbpf("Auto1:m4.RTRY", "0")
+dbpf("Auto1:m4.TWV", "0.1")
 
 # Boot complete
