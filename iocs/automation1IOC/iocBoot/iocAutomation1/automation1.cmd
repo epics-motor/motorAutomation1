533e15941d144d2031e55871e74dd0511e64f78c
Kevin Peterson <kmpeters@anl.gov>
2021-10-11 15:34:46 -0500

    Added an asyn record and example asyn trace config

--- iocs/automation1IOC/iocBoot/iocAutomation1/automation1.cmd
+++ iocs/automation1IOC/iocBoot/iocAutomation1/automation1.cmd
@@ -1,7 +1,14 @@
 
 Automation1CreateController("Automation1", "127.0.0.1", 4, 100, 1000)
 
+# traceError and traceIODriver
+#!asynSetTraceMask("Automation1", 0, 0x9)
+# traceIOAscii
+#!asynSetTraceIOMask("Automation1", 0, 0x1)
+
 Automation1CreateProfile("Automation1", 2000)
 
 dbLoadTemplate "motor.substitutions.automation1"
+
+dbLoadRecords("$(ASYN)/db/asynRecord.db", "P=Auto1:, R=asyn1, PORT=Automation1, ADDR=0, OMAX=256, IMAX=256")
 
