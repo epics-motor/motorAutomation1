88ae808c1e9f6fde135e20164988f1a68fb40243
Kevin Peterson <kmpeters@anl.gov>
2022-03-28 15:45:39 -0500

    Added startlocal & endlocal to the IOC batch script to avoid appending the path each time the IOC is started

--- iocs/automation1IOC/iocBoot/iocAutomation1/start_IOC.bat
+++ iocs/automation1IOC/iocBoot/iocAutomation1/start_IOC.bat
@@ -1,5 +1,7 @@
 @echo off
 
+setlocal
+
 call dllPath.bat
 
 ..\..\bin\%EPICS_HOST_ARCH%\automation1.exe st.cmd
@@ -7,3 +9,4 @@ call dllPath.bat
 REM Allow error messages to be read before the cmd window disappears
 pause
 
+endlocal
