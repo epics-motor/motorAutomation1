9d44d4b55a6caf2b585d657c8561960988eb2f4a
Mark Rivers <rivers@cars.uchicago.edu>
2026-03-21 16:33:49 -0500

    Add enablePCO method; add programPosition_ variable, needed for PCO

--- automation1App/src/Automation1MotorAxis.h
+++ automation1App/src/Automation1MotorAxis.h
@@ -28,6 +28,7 @@ public:
     asynStatus setPosition(double position);
     asynStatus setClosedLoop(bool closedLoop);
     asynStatus defineProfile(double *positions, size_t numPoints);
+    asynStatus enablePCO(bool enable);
 
 private:
     // Pointer to asynMotorController to which the axis belongs.
@@ -43,6 +44,7 @@ private:
     void logError(const char* driverMessage);
     
     double countsPerUnitParam_;
+    double programPosition_;
     
     double profilePreDistance_;
     double profilePrePosition_;
